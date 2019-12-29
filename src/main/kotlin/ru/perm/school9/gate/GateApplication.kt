29d163a96b99629202b8828fd65d59627e222e2d
Yankovskiy Evgeniy <yankovskiy-se@dartit.ru>
2019-12-29 06:40:52 +0500

    fix TesterCommunicationService

--- src/main/kotlin/ru/perm/school9/gate/GateApplication.kt
+++ src/main/kotlin/ru/perm/school9/gate/GateApplication.kt
@@ -1,7 +1,10 @@
 package ru.perm.school9.gate
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
 
 @SpringBootApplication
 class GateApplication
--- src/main/kotlin/ru/perm/school9/gate/service/TesterCommunicationService.kt
+++ src/main/kotlin/ru/perm/school9/gate/service/TesterCommunicationService.kt
@@ -3,25 +3,21 @@ package ru.perm.school9.gate.service
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.FuelManager
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Service
-import ru.perm.school9.gate.exception.TesterCommunicationException
 import ru.perm.school9.gate.model.Submit
-import java.net.URL
 import javax.annotation.PostConstruct
 
 @Service
-@ConfigurationProperties("tester")
 class TesterCommunicationService {
-    @Value("\${host}")
+    @Value("\${tester.host}")
     private lateinit var host: String
 
-    @Value("\${port}")
+    @Value("\${tester.port}")
     private lateinit var port: String
 
-    @Throws(TesterCommunicationException::class)
     fun sendSubmitForTesting(submit: Submit) {
         //TODO
+        // decide if there is any authentication needed
         val response = Fuel.post("").body(submit.id!!).response()
         println("Request to test submit ${submit.id!!} has been set.\n Tester has responded with: $response.")
     }
