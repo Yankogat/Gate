ab3e869a247c6d676d5bf61b423db0281f3aaef6
Eugene Y <yankogat@gmail.com>
2020-01-22 20:38:26 +0500

    removed tests and test groups from problem

--- src/main/kotlin/ru/perm/school9/gate/model/Problem.kt
+++ src/main/kotlin/ru/perm/school9/gate/model/Problem.kt
@@ -2,7 +2,6 @@ package ru.perm.school9.gate.model
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
-import org.springframework.data.mongodb.core.mapping.Field
 
 @Document(collection = "Problems")
 data class Problem (
@@ -10,9 +9,5 @@ data class Problem (
         var id: String?,
         var name: String?,
         var wording: String?,
-        var tests: List<Test>?,
-        var testGroups: List<TestGroup>?,
-        var checkerId: String?,
-        var conjuctive: Boolean?,
         var value: Int?
 )
--- src/main/kotlin/ru/perm/school9/gate/model/Test.kt
+++
@@ -1,7 +0,0 @@
-package ru.perm.school9.gate.model
-
-data class Test (
-        var input: String?,
-        var answer: String?,
-        var value: Int?
-)
